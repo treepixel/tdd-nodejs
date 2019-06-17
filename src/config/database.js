e20a6e8e122be29c1c7fe24df200dec74b80055f
Antônio Rodrigues <antonio.rodrigues_tga@hotmail.com>
2019-06-17 15:32:33 -0300

    Set Up initial configs and dependencies to up express with sequelize and postgres

---
+++ src/config/database.js
@@ -0,0 +1,18 @@
+require("dotenv").config({
+  path: process.env.NODE_ENV === "test" ? ".env.test" : ".env"
+});
+
+module.exports = {
+  host: process.env.DATABASE_HOST,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  dialect: process.env.DATABASE_DIALECT || "postgres",
+  operatorsAliases: false,
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+};
